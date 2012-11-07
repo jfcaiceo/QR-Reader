64adb50f007b1804a3f5cbbfbb5739b6188ee112
Francisco Caiceo <jfcaiceo55@gmail.com>
2012-11-06 21:47:28 -0300

    borrado price reader

--- QR-Application/src/Program.java
+++ QR-Application/src/Program.java
@@ -6,6 +6,9 @@ public class Program {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//Cambiar servidor o cliente en QRApp
+		
 		Application app = new QRApp();
 		app.run();
 	}
--- QR-Application/src/QRApp.java
+++ QR-Application/src/QRApp.java
@@ -3,17 +3,21 @@ public class QRApp extends Application{
 	private QRController controller;
 	
 	public void run(){
-		/*
+		/*Metodos de device no implementados todavia
+		 * 
 		//Si es notebook, queda como servidor
 		if(this.currentDevice.getType() == 0){
-			controller = new ServerController();
+			controller = new ServerData();
 		}
 		//Si no, queda como cliente
 		else{
 			controller = new ClientController();
 		}
 		*/
-		controller = new ClientController();
+		
+		//controller = new ClientController();
+		controller = new ServerData();
+		
 		controller.start();
 	}
 }
